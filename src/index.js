7be17280b58b025bbaa8bd4adb27b5adcfc3a556
Vo Nhat Truong <vonhattruong250695@gmail.com>
2019-02-25 22:53:07 +0700

    Update file system nodejs module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-let fileManager = require('./fileManager');
+let fileManager;
+fileManager = require('./fileManager');
 const file = __dirname+'/../data/test.txt';
-const newFileContent = 'File này đã được chỉnh sửa toàn bộ';
+// const newFileContent = 'File này đã được chỉnh sửa toàn bộ';
 
 console.log('working with file');
 
-// fileManager.createNewFile(file,fileContent);
-// fileManager.readFile(file);
-// fileManager.updateFile(file,newFileContent);
-fileManager.deleteFile(file);
+/*
+ fileManager.createNewFile(file,fileContent);
+ fileManager.readFile(file);
+ fileManager.updateFile(file,newFileContent);
+*/
+// fileManager.deleteFile(file);
