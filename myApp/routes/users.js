a75d58c396529b7ed5370979630850d3a1902f6a
Vo Nhat Truong <vonhattruong250695@gmail.com>
2019-03-05 23:09:17 +0700

    Finish connect to mongo database and send request to server

--- myApp/routes/users.js
+++ myApp/routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resource 3141243214');
+});
+
+router.post('/testPost', function(req, res, next) {
+  res.send('Post method return response');
 });
 
 module.exports = router;
