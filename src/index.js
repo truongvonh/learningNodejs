cc9b35d03711a5aedec0acfec58fc85a27f82f12
Vo Nhat Truong <truongvn@bap.jp>
2019-05-01 23:02:27 +0700

    [be] add list all task and create task with check value by middlewares

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import db from './db/models'
+import taskRouter from './routes/task.routes'
+
 const app = express()
-const port = 4000
+const port = 5000 
+const sequelize = db.sequelize
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.send('Hello World! abcd'))
+app.use('/task', taskRouter)
+
+app.listen(port, 
+  () => {
+    sequelize
+      .sync({ force: false })
+      .then(msg => msg)
+  }
+)
